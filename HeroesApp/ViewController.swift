9ea9983f0e62012d04d1f48bd488096ab14165e7
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-17 13:45:46 +0300

    Initial Commit

---
+++ HeroesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroesApp
+//
+//  Created by Alex Kulish on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
