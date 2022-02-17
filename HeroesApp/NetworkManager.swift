423572ca68a3ad2c6b8eceac02f172638ca177cd
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-17 15:54:33 +0300

    Add NetworkManager

---
+++ HeroesApp/NetworkManager.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkManager.swift
+//  HeroesApp
+//
+//  Created by Alex Kulish on 17.02.2022.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidUrl
+    case noData
+    case decodingError
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(completion: @escaping(Result<[Hero], NetworkError>) -> Void) {
+        
+        guard let url = URL(string: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.2.0/api/all.json") else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description ")
+                return
+            }
+            
+            do {
+                let heroes = try JSONDecoder().decode([Hero].self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(heroes))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+    
+    func fetchImage(from url: URL, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description ")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(data))
+            }
+        }.resume()
+    }
+}
